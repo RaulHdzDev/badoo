dfc21aa3e05007fcbb6c14f9db0c942e0017685e
Jose Kabo <kabo.0699@gmail.com>
2020-06-07 10:33:39 -0500

    Apenas login

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:badoo/Presentacion/Pages/Login/LoginPage.dart';
+import 'package:badoo/Presentacion/Pages/Splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
  
@@ -7,17 +11,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+          child: Splash()
         ),
       ),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  
+ @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      ()  {
+        Navigator.pushReplacement(
+            this.context,
+            MaterialPageRoute(
+              builder: (context) => HomePage(),
+            ),
+          );
+      },
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: SplashScreenBadoo(),
     );
   }
 }
